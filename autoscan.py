db79d3bd99c0ab52894358a0560e5e42994b4d64
rain <102387043+senseIess@users.noreply.github.com>
2022-04-07 01:57:36 -0700

    Update autoscan.py

--- autoscan.py
+++ autoscan.py
@@ -2,20 +2,25 @@ import os
 import colorama
 from subprocess import call
 from colorama import Fore, Style
+import datetime
+from googlesearch import search, get_tbs
 
-print(Fore.YELLOW +"""   
-                      88              
-                      ""              
-                                      
-8b,dPPYba, ,adPPYYba, 88 8b,dPPYba,   
-88P'   "Y8 ""     `Y8 88 88P'   `"8a  
-88         ,adPPPPP88 88 88       88  
-88         88,    ,88 88 88       88  
-88         `"8bbdP"Y8 88 88       88  
+colorama.init(autoreset=True)
+
+
+print(Fore.LIGHTGREEN_EX +""" 
+========================================
+           _       
+          (_)      
+ _ __ __ _ _ _ __  
+| '__/ _` | | '_ \ 
+| | | (_| | | | | |
+|_|  \__,_|_|_| |_|
+discord: rain#1000                
 """)
 
-print(Fore.GREEN +"""
-[1] advanced nmap scan with nmap 
+print(Fore.LIGHTYELLOW_EX +"""
+[1] advanced scan with nmap 
 [2] directory hunter with gobuster
 [3] admin login hunter with gobuster
 [4] check vulns with nikto 
@@ -24,15 +29,16 @@ print(Fore.GREEN +"""
 [7] find info about target with whois 
 [8] find exploits with searchsploit
 
-// SQLMAP 
+// SQLMAP
 
-[9] sqli with sqlmap 
-[10] dbs, --tables
-[11] dbs, tables, --dump
-[12] sql with sqlmap (advanced mode) 
+[D] google hacking / dorks
+[S] sqli with sqlmap 
+[S2] dbs, --tables
+[S3] dbs, tables, --dump
+[S4] sql with sqlmap (advanced mode) 
 
 """)
-at = input("number => ")
+at = input(Fore.LIGHTGREEN_EX +"option => ")
 if at == "1" :
    atnmap = input("ip or url => ") 
    os.system("sudo nmap -sV -O -Pn -sS " + atnmap + "")
@@ -59,21 +65,37 @@ if at == "7" :
 if at == "8" :
    atss = input("exploit => ")
    os.system("searchsploit " + atss +"")
-if at == "9":
+if at == "S"
    atsq1 = input("url => ")
    call(["sqlmap", atsq1 ,"--dbs", "--random-agent"])
    print(Fore.RED + '[URL]' + atsq1)
-if at == "10" :
+if at == "S2" :
    atsq3 = input("url => ")
    atdbn = input("database name => ") 
    call(["sqlmap", atsq3 ,"-D", atdbn, "--tables", "--random-agent"])
    print(Fore.RED + '[URL]', atsq3 + '\n' + '[DBS]', atdbn)
-if at == "11" :
+if at == "S3" :
    atsq4 = input("url => ")
    atdbn2 = input("database name => ") 
    attb = input("table name => ") 
    call(["sqlmap", atsq4,"-D", atdbn2, "-T", attb, "--dump", "--random-agent"])
    print(Fore.RED + '[URL]', atsq4 + '\n' + '[DBS]', atdbn2 + '\n' + '[TAB]', attb)
-if at == "12" :
+if at == "S4" :
    atsq2 = input("url =>")
    call(["sqlmap", atsq2 ,"--tamper=space2comment,between,space2plus", "-v 2", "--hex", "--random-agent", "--skip-waf", "--risk=3", "--level=3"])
+if at == "D" :
+   searchterm = input(f'{Fore.GREEN}dork => {Fore.WHITE}')
+   searchnumber = input(f'{Fore.GREEN}how many websites do u want? => {Fore.WHITE}')
+   searchnumber = int(searchnumber)
+
+   for i in search(
+    searchterm, 
+    tld="com", 
+    num=searchnumber, 
+    stop=searchnumber, 
+    pause=2,
+    tbs=get_tbs(
+        datetime.date(2021, 1, 1),
+        datetime.date(2022, 4, 1))
+   ):
+    print(Fore.RED + '[!]', Fore.LIGHTBLUE_EX + i)
